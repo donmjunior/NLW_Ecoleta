7edeb7cdc906ea8ca71d07a286139db6c5a6c1f2
Donizeti Leite <donmjunior@gmail.com>
2020-06-03 13:55:11 -0300

    Aulas dos dias 2 e 3 do evento NLW

---
+++ New/scripts/index.js
@@ -0,0 +1,5 @@
+function changePage(event){
+    window.location.href = "http://127.0.0.1:5500/create-point.html"
+}
+
+document.querySelector("a[name=botao]").addEventListener("change", changePage)
